b0c5e44c9d02fa13f31559ce7814984a85be9ded
Jose Luis Salas <josacar@users.noreply.github.com>
2025-03-07 16:13:00 +0100

    Use LazyVim for neovim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,2 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-require("config.options")
-
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-  -- disable luarocks if it is not available on system
-  -- rocks = { enabled = false },
-})
- 
-require('lspconfig').terraformls.setup{}
