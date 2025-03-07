6c6630a2ac432ac50872ffc287613482cf0d6bca
Jose Luis Salas <josacar@users.noreply.github.com>
2025-03-07 17:47:53 +0100

    [master] Add more treesitter plugins in neovim
    Signed-off-by: Jose Luis Salas <josacar@users.noreply.github.com>

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "hcl",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "terraform",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
