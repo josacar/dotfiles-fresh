2eaf07c95ab15fd8ca57ebebadce00ebece4043b
Jose Luis Salas <josacar@users.noreply.github.com>
2025-03-27 17:47:26 +0100

    [master] Add codecompanion to neovim
    Signed-off-by: Jose Luis Salas <josacar@users.noreply.github.com>

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -22,4 +22,22 @@ return {
       },
     },
   },
+  {
+    "olimorris/codecompanion.nvim",
+    config = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "gemini",
+        },
+        inline = {
+          adapter = "gemini",
+        },
+      },
+    },
+  },
 }
